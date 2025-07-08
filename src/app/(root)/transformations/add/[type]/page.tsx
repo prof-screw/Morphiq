56321544bb319a8dc00613eb15a7a503d8c9fb5d
prof-screw <raunak.thapa2021@gmail.com>
2025-07-08 18:07:07 +0545

    Transform form UI

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,31 +1,26 @@
 //based on route/ type of your transformation, your transformation object will 
 // pull from transformationtypes and auto fill the title and subtitles
-// Define the type for the resolved params
-type ResolvedParams = {
-  id: string;
-  type: TransformationTypeKey;
-};
-
-// Update SearchParamProps to expect params as a Promise
-type SearchParamProps = {
-  params: Promise<ResolvedParams>;
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs/server";
 
+type SearchParamProps = {
+  params: {
+    id: string;
+    type: TransformationTypeKey;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
 const AddTransformationTypePage = async ({ params }: SearchParamProps) => {
-  // Await the params to resolve the Promise
-  const { type } = await params;
+  const { id, type } = params;
   const { userId, redirectToSignIn } = await auth();
-  const transformation = transformationTypes[type];
 
   if (!userId) return redirectToSignIn();
 
+  const transformation = transformationTypes[type];
   const user = await getUserById(userId);
 
   return (
